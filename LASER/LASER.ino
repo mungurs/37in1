4394d4f79c0973c30a70652f5671d36f0671ead3
Mārtiņš Ungurs <mungurs@gmail.com>
2016-12-30 00:48:55 +0200

    Add laser code

---
+++ LASER/LASER.ino
@@ -0,0 +1,18 @@
+// Henry's Bench
+// KY-008 Tutorial... On then Off
+
+
+int laserPin = 10;
+
+void setup ()
+{
+   pinMode (laserPin, OUTPUT); // define the digital output interface 13 feet
+}
+void loop () {
+   digitalWrite (laserPin, HIGH); // Turn Laser On
+   delay (50); // On For Half a Second
+   digitalWrite (laserPin, LOW); // Turn Laser Off
+   delay (50); // Off for half a second
+}
+
+
